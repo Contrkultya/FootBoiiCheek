29b9e5a1728e26dd6c4f5172fa247ddddeab6033
Maria1808 <mvbannikova1808@gmail.com>
2022-02-16 22:56:49 +0500

    Update manage-execution.js
    Стало покрасивее

--- src/pages/manage-execution.js
+++ src/pages/manage-execution.js
@@ -5,34 +5,79 @@ const ManageExecution = () => {
    const history = useNavigate()
     //насколько я понимаю yes/no должны откуда-то подгружаться, но я хз откуда
     return (
-        <div className='flex items-center flex-col text-center'>
-            <div className=" mr-28 ml-28 font-tahoma">
-                <div className="flex flex-row justify-between mt-28 space-y-18">
-                    <div className="flex flex-col space-x-3">
-                        <button className="bg-mustard hover:bg-bear text-black font-bold py-2 flex-1">Allocate Teams to Groups...</button>
-                    </div>
-                    <div className="flex flex-col">
-                        <p className='font-tahoma font-bold'>Teams alloceted</p>
-                        <p>yes</p>
-                    </div>
-                </div>
-                <div className="flex flex-row justify-between mt-12 space-y-8">
-                    <div className="flex flex-col">
-                        <button onClick={() => history('/manage-games')} className="bg-mustard hover:bg-bear text-black font-bold py-2 flex-1">Manage Groups Stage Games...</button>
-                        <button className="bg-mustard hover:bg-bear text-black font-bold py-2 flex-1">Manage Round of 16 Games...</button>
-                        <button className="bg-mustard hover:bg-bear text-black font-bold py-2 flex-1">Manage Quarter-Final Games...</button>
-                        <button className="bg-mustard hover:bg-bear text-black font-bold py-2 flex-1">Manage Semi-Final Games...</button>
-                        <button className="bg-mustard hover:bg-bear text-black font-bold py-2 flex-1">Manage Final Games...</button>
-                    </div>
-                    <div className="flex flex-col">
-                        <p className='font-tahoma font-bold'>Finished</p>
-                        <p>yes</p>
-                        <p>no</p>
-                        <p>no</p>
-                        <p>no</p>
-                        <p>no</p>
-                    </div>
-                </div>
+        <div className='flex text-center mx-auto max-w-6xl mt-12'>
+            <div className="font-tahoma mx-auto">
+                <table>
+                    <thead>
+                        <tr>
+                            <th></th>
+                            <th>&nbsp;&nbsp;&nbsp;Teams alloceted</th> 
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td><button className="bg-mustard hover:bg-bear text-black font-bold py-2 min-w-full flex-1">Allocate Teams to Groups...&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</button></td>
+                            <td><p>yes</p></td>
+                        </tr>
+                    </tbody>
+                </table>
+                <table>
+                    <thead>
+                        <tr>
+                            <th></th>
+                            <th>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                                Finish
+                            </th> 
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td><button onClick={() => history('/manage-games')} className="bg-mustard hover:bg-bear text-black font-bold py-2 min-w-full flex-1">Manage Groups Stage Games...</button></td>
+                            <td>
+                                <p>
+                                    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                                    yes
+                                </p>
+                            </td>
+                        </tr>  
+                        <tr>
+                            <td><button className="bg-mustard hover:bg-bear text-black font-bold py-2 min-w-full flex-1">Manage Round of 16 Games...</button></td>
+                            <td>
+                                <p>
+                                    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                                    yes
+                                </p>
+                            </td>
+                        </tr>  
+                        <tr>
+                            <td><button className="bg-mustard hover:bg-bear text-black font-bold py-2 min-w-full flex-1">Manage Quarter-Final Games...</button></td>
+                            <td>
+                                <p>
+                                    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                                    yes
+                                </p>
+                            </td>
+                        </tr>  
+                        <tr>
+                            <td><button className="bg-mustard hover:bg-bear text-black font-bold py-2 min-w-full flex-1">Manage Semi-Final Games...</button></td>
+                            <td>
+                                <p>
+                                    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                                    yes
+                                </p>
+                            </td>
+                        </tr>  
+                        <tr>
+                            <td><button className="bg-mustard hover:bg-bear text-black font-bold py-2 min-w-full flex-1">Manage Final Games...</button></td>
+                            <td>
+                                <p>
+                                    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                                    yes
+                                </p>
+                            </td>
+                        </tr>  
+                    </tbody>
+                </table>
             </div>
         </div>
     );
