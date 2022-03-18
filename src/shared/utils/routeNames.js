7ae49d2c116e0ea42624166d53be5f4d52c3bf8b
ismakova <ismakova.dayana@mail.ru>
2022-03-19 03:01:48 +0500

    Changed manage execution and made common form for: Manage Group Stage Games, Manage Round of 16 Games etc.

--- src/shared/utils/routeNames.js
+++ src/shared/utils/routeNames.js
@@ -4,5 +4,5 @@ export const ADD_EDIT_TEAM = '/add-edit-team'
 export const MANAGE_TOURNAMENTS = '/tournaments'
 export const GAME_FORM_ROUTE = '/tournaments/:tournamentID/game/:gameID';
 export const MANAGE_EXECUTION = '/manage-execution'
-export const MANAGE_GAMES = '/manage-games'
+export const MANAGE_GAMES = '/manage-games/:id'
 export const ALLOCATE_TEAMS = '/allocate-teams'
